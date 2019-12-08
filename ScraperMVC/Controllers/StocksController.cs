25054574fcec107536dedf161e0504cb59e9754c
Daniel Choiniere <dpchoiniere@gmail.com>
2019-12-08 05:15:53 +0000

    creating all necessary routes

---
+++ ScraperMVC/Controllers/StocksController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ScraperMVC.Models;
+
+namespace ScraperMVC.Controllers
+{
+    public class StocksController : Controller
+    {
+//        GET: stocks/random
+        public IActionResult Random()
+        {
+            var stock = new Stock() {Symbol = "BTC"};
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+//        stocks
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+    }
+}
---
+++ ScraperMVC/Models/Stock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ScraperMVC.Models
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Stock> Stocks { get; set; }
+    }
+
+    public class Stock
+    {
+        public int id { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
+        public string Symbol { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
+        public string LastPrice { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
+        public string Change { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
+        public string Currency { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
+        public string DataCollectedOn { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
+        public string Volume { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
+        public string AvgVol3m { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
+        public string MarketCap { get; set; }
+    }
+}
